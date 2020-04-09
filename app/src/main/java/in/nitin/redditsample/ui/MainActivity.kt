c9856bbe3000c733db72609cfd39948d874d0114
nitinprakash <ashu9911>
2020-04-09 18:07:31 +0530

    Post response data bind with views and some UI changes has made for post in activity_main.xml, color.xml and we also added extension functions for timeConverter, image show & hide and snackBar in ExtensionFunc.kt

--- app/src/main/java/in/nitin/redditsample/ui/MainActivity.kt
+++ app/src/main/java/in/nitin/redditsample/ui/MainActivity.kt
@@ -3,14 +3,19 @@ package `in`.nitin.redditsample.ui
 import `in`.nitin.redditsample.R
 import `in`.nitin.redditsample.application.RedditApplication
 import `in`.nitin.redditsample.databinding.ActivityMainBinding
+import `in`.nitin.redditsample.datasource.model.Data_
 import `in`.nitin.redditsample.datasource.network.Result
 import `in`.nitin.redditsample.viewmodel.RedditViewModel
 import `in`.nitin.redditsample.viewmodel.ViewModelProviderFactory
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.content_layout.view.*
 import javax.inject.Inject
 
 
@@ -55,25 +60,42 @@ class MainActivity : AppCompatActivity() {
                 when (it.status) {
 
                     Result.Status.LOADING -> {
-
-                        binding.statusTv.text = "Loading"
-
+                        binding.contentLayout.status_image.show()
+                        binding.btnLoadPost.isClickable = false
                     }
                     Result.Status.SUCCESS -> {
+                        subscribeUi(
+                            it.data!![0].data!!.children!![0].data
+                        )
 
-                        binding.statusTv.text = "Success"
-
+                        binding.contentLayout.status_image.hide()
+                        binding.btnLoadPost.isClickable = true
 
                     }
                     Result.Status.ERROR -> {
-                        binding.statusTv.text = it.message
-
+                        binding.contentLayout.status_image.hide()
+                        binding.btnLoadPost.isClickable = true
+                        it.message!!.snack(Color.RED, binding.root)
 
                     }
                 }
             }
         })
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun subscribeUi(data_: Data_?) {
+        binding.contentLayout.titleTv.text = data_!!.title!!.trim()
+        binding.contentLayout.dateTv.text = data_.created!!.toLong().timeConverter()
+        binding.contentLayout.authorTv.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+        binding.contentLayout.authorTv.text = data_.author!!.trim()
+        binding.contentLayout.selfTextTv.text =
+            data_.selftext?.trim()!!.ifEmpty { "No Self Text" }
+        binding.contentLayout.numCommentsTv.text = "${data_.numComments.toString()} comments"
+        binding.contentLayout.likesTv.text =
+            "${if (data_.likes == null) "0" else data_.likes} likes"
 
     }
+
 
 }
